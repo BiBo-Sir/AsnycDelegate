259e65d173e7f969c9a99fe9d0b79e6be9fa1d3e
henry <32626884+Henry-GG@users.noreply.github.com>
2018-11-20 16:42:24 +0800

    Update Code.cs
    异步委托作用：当前的异步委托方法在新的线程上执行，可直接跳过委托的代码块在主线程上继续执行下面的代码

--- Code.cs
+++ Code.cs
@@ -5,7 +5,7 @@ namespace AsnycDelegateTest2
     /// <summary>
     /// .net framework支持异步调用方法，BeginInvoke 和EndInvoke是委托自动生成的方法
     /// 
-    /// BeginInvoke启用异步调用,该方法有与异步执行的方法相同的参数，另外还有两个可选参数，
+    /// BeginInvoke启用异步调用,该方法有与异步执行的方法相同的参数，另外还有两个可选参数。
     /// 第一个参数是AsyncCallback委托，该委托引用在异步调用完成时要调用的方法。
     /// 第二个参数是用户自定义的对象，该对象将信息传递给回调方法。
     /// BeginInvoke返回一个IAsyncResult，后者可用于监视异步调用的进度。
